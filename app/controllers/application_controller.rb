a77d86fb19790a059f815a658acbfc433cb71477
s-maaya <m.2212s.a@gmail.com>
2019-11-21 17:26:02 +0000

    時間修正で一時保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,15 @@ class ApplicationController < ActionController::Base
             rooms_path
         end
     end
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        #ユーザー登録（sign_up)の際にユーザー名（neme)のデータ操作が許可される。
+    end
+
     # def after_sign_out_path_for(resource)
     #     # case resource
     #     # when AdminUser
