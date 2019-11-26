f90cda1913b4f7b0d54aa7a4b6b4727b51395c2b
s-maaya <m.2212s.a@gmail.com>
2019-11-26 15:39:34 +0900

    install mysql2 and edit puma.rb

--- app/controllers/end_user/base_controller.rb
+++
@@ -1,5 +0,0 @@
-class EndUser::BaseController < ApplicationController
-    layout 'end_user'
-
-
-end
--- app/models/message.rb
+++
@@ -1,5 +0,0 @@
-class Message < ApplicationRecord
-    belongs_to :room
-    belongs_to :user
-    after_create_commit { MessageBroadcastJob.perform_later self }
-end
