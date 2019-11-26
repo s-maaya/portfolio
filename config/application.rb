0c1090632d336b4e4f217177ce623215b794b31b
s-maaya <m.2212s.a@gmail.com>
2019-11-26 20:53:42 +0900

    edit config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips = '10.0.2.2'
+    unless Rails.env.production?
+      config.web_console.whitelisted_ips = '10.0.2.2'
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
